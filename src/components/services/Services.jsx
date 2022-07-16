5e7fd950eb749cf54726d19c20cd4973aac3d7ce
carlen32 <108589578+carlen32@users.noreply.github.com>
2022-07-16 11:15:43 -0700

    Services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
    <section id='services'>
     <h5>What I Offer</h5>
     <h2>Services</h2>
-    <div className="container services_container">
+    <div className="container services__container">
 <article className="service">
   <div className="service__head">
     <h3>UI/UX Design</h3>
@@ -26,6 +26,47 @@ const Services = () => {
   </li>
   </ul>
 </article>
+{/* End of UI/UX */}
+<article className="service">
+  <div className="service__head">
+    <h3>Web Development</h3>
+  </div>
+  <ul className='service__list'>
+  <li>
+    <AiOutlineCheck className='service__list-icon' />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+  </li>
+  <li>
+    <AiOutlineCheck className='service__list-icon' />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+  </li>
+  <li>
+    <AiOutlineCheck className='service__list-icon' />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+  </li>
+  </ul>
+</article>
+{/* End OF WEB DEVELOPMENT */}
+<article className="service">
+  <div className="service__head">
+    <h3>Content Creation</h3>
+  </div>
+  <ul className='service__list'>
+  <li>
+    <AiOutlineCheck className='service__list-icon' />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+  </li>
+  <li>
+    <AiOutlineCheck className='service__list-icon' />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+  </li>
+  <li>
+    <AiOutlineCheck className='service__list-icon' />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+  </li>
+  </ul>
+</article>
+{/* End of Content*/}
     </div>
    </section>
   )
